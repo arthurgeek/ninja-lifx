04018b6d519e66fa4d4180c67d5fb41263b04871
Arthur Zapparoli <arthurzap@gmail.com>
2014-09-21 01:07:16 -0300

    Using lifx-http

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 var util = require('util');
 var stream = require('stream');
-var lifx = require('lifx');
-//lifx.setDebug(true);
+var http = require('http');
+var lds = require('lodash');
+var options = {
+  host: '10.0.1.2',
+  port: 56780,
+  path: '/lights.json'
+}
 
 util.inherits(Driver, stream);
 util.inherits(Bulb, stream);
@@ -20,70 +25,92 @@ function Driver(opts,app) {
 
   app.once('client::up', function() {
     self.log.info('Firing up!');
-    var lx = lifx.init();
 
-    lx.on('bulb', function(bulb) {
-      self.log.info('New bulb found: ', bulb);
+    http.get(options, function(resp){
+      resp.on('data', function(body){
+        body = JSON.parse(body);
 
-      var device  = new Bulb(bulb, lx);
-      if (!devices[device.G]) {
-        self.log.info('Registering bulb');
-        devices[device.G] = device;
+        lds.each(body, function(bulb) {
+          self.log.info('New bulb found: ', bulb['label']);
 
-        self.emit('register', device);
-      }
+          var device  = new Bulb(bulb);
+          if (!devices[device.G]) {
+            self.log.info('Registering bulb');
+            devices[device.G] = device;
+
+            self.emit('register', device);
+          }
+        });
+      });
+    }).on("error", function(e){
+      console.log("Got error: " + e.message);
     });
   });
 }
 
-function Bulb(bulb, lx) {
-
+function Bulb(bulb) {
   var self = this;
-
-  this.lx = lx;
   this.bulb = bulb;
-
   this.writeable = true;
   this.readable = true;
   this.V = 0;
   this.D = 1008;
-  this.G = 'Lifx' + bulb.lifxAddress.toString('hex');
-  this.name = 'Lifx - ' + (bulb.name||'(No Name)');
+  this.G = 'Lifx' + this.bulb['id'];
+  this.name = 'Lifx - ' + (this.bulb['label']||'(No Name)');
 
-  this.lx.on('packet', function(p) {
-    if (p.preamble.bulbAddress.toString('hex') === this.bulb.lifxAddress.toString('hex')) {
+  http.get(options, function(resp) {
+    resp.on('data', function(body) {
+      body = JSON.parse(body);
 
-      if (p.packetTypeShortName == 'lightStatus') {
-
-        if (this.log) this.log.trace('Light status', p);
-
-        this.emit('data', {
-          hue: p.payload.hue,
-          sat: p.payload.saturation / 256,
-          bri: p.payload.brightness / 256,
-          on: p.payload.power > 0
-        });
-      }
-    }
-  }.bind(this));
-
+      lds.each(body, function(bulb) {
+        if (bulb['id'] === self.bulb['id']) {
+          self.emit('data', {
+            hue: bulb['color']['hue'],
+            sat: bulb['color']['saturation'],
+            bri: bulb['color']['brightness'],
+            on: bulb['on'],
+          });
+        }
+      });
+    });
+  }).on("error", function(e){
+    console.log("Got error: " + e.message);
+  });
 }
 
 Bulb.prototype.write = function(data) {
+  var self = this;
+
   if (typeof data === 'string') {
     data = JSON.parse(data);
   }
 
   if (!data.on) {
-    this.lx.lightsOff(this.bulb);
+    options['method'] = 'PUT';
+    options['path'] = '/lights/' + self.bulb['id'] + '/off';
+
+    http.request(options, function(res) {
+      console.log('Light Off' + self.bulb['label']);
+    }).end();
   }
 
   if (data.hue && data.sat && data.bri) {
-    this.lx.lightsColour(data.hue, data.sat*256, data.bri*256, 0x0dac/*TODO*/, data.transitiontime||200, this.bulb);
+    options['method'] = 'PUT';
+    options['path'] = '/lights/' + self.bulb['id'] + '/color?hue=' + data.hue +
+      '&saturation=' + data.sat + '&brightness=' + data.bri + '&duration=' +
+      data.transitionTime || 2;
+
+    http.request(options, function(res) {
+      console.log('Light Colored' + self.bulb['label']);
+    }).end();
   }
-  
+
   if (data.on) {
-    this.lx.lightsOn(this.bulb);
+    options['method'] = 'PUT';
+    options['path'] = '/lights/' + self.bulb['id'] + '/on';
+
+    http.request(options, function(res) {
+      console.log('Light On' + self.bulb['label']);
+    }).end();
   }
 };
-
--- test.js
+++ test.js
@@ -6,39 +6,31 @@ var opts = {};
 
 var app = new EventEmitter();
 app.log = {
-    debug: console.log,
-    info: console.log,
-    warn: console.log,
-    error: console.log
+  debug: console.log,
+  info: console.log,
+  warn: console.log,
+  error: console.log
 };
 
 var driver = new (require('./index'))(opts, app);
 driver.log = app.log;
 
 driver.on('register', function(device) {
-    console.log('Driver.register', device);
-    device.on('data', function(value) {
-        console.log('Device.emit data', value);
-    });
-    if (device.D == 1008) { //It's a light
-      setInterval(function() {
-         device.write({bri:254,sat:254,hue:Math.floor(Math.random()* 65535),on:true,transitionTime:0});
-      }, 200);
-    }
-/*
-    if (device.D == 238) { //It's a relay
-      var x = false;
-      setInterval(function() {
-         device.write(x=!x);
-      }, 1000);
-    }*/
-
+  console.log('Driver.register', device);
+  device.on('data', function(value) {
+    console.log('Device.emit data', value);
+  });
+  if (device.D == 1008) { //It's a light
+    setInterval(function() {
+      device.write({bri:254,sat:254,hue:Math.floor(Math.random()* 65535),on:true,transitionTime:0});
+    }, 200);
+  }
 });
 
 driver.save = function() {
-    console.log('Saved opts', opts);
+  console.log('Saved opts', opts);
 };
 
 setTimeout(function() {
-    app.emit('client::up');
+  app.emit('client::up');
 }, 500);
